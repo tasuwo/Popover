89f2bc7bda93c041e62f08b6ed65c9a3e027551b
Tasuku Tozawa <15nm722x@vc.ibaraki.ac.jp>
2016-10-29 13:28:46 +0900

    Initial commit

---
+++ MenubarApplication/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenubarApplication
+//
+//  Created by tasuku tozawa on 2016/10/29.
+//  Copyright © 2016年 tasuwo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
